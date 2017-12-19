23c16ce72369c6e8128a931790f0686d7e8e9f4a
sagiannaki <33251155+sagiannaki@users.noreply.github.com>
2017-12-19 12:58:30 +0200

    Add files via upload
    teleutaio

--- babliakis/src/babliakis/Plane.java
+++ babliakis/src/babliakis/Plane.java
@@ -1,6 +1,8 @@
 package babliakis;
 
-public class Plane {
+import java.util.ArrayList;
+
+public class Plane  {
 	Point3D[] points;
 	
 	public Plane(){
@@ -43,5 +45,41 @@ public class Plane {
 		}
 		return points[index];
 	}
+	
+	Point3D[]insideSquare(Point leftUp, Point leftDown, Point rightUp,Point rightDown){
+		ArrayList<Point3D> inside=new ArrayList<Point3D>();
+		for (int i=0;i<points.length;i++){
+			if (points[i].x>leftDown.x && points[i].x<rightDown.x &&
+					points[i].y>leftDown.y && points[i].y<leftUp.y){
+				inside.add (points[i]);
+				
+			}
+		}
+		Point3D[] inArray =new Point3D[inside.size()];
+		for (int i=0; i <inArray.length; i++){
+			inArray[i]=inside.get(i);
+		}
+		return inArray;
+		
+	}
+	
+	public static void main(String[] args){
+		Plane plane =new Plane(100);
+		for(int i=0; i<plane.points.length;i++){
+			plane.points[i]=new Point3D((int)(Math.random()*200),
+					
+					(int)(Math.random()*200), (Math.random()*20));
+					}
+		
+		System.out.println(plane.closestPoint(new Point(100,100)) + 
+				","+plane.closestPoint(new Point(100,100)) );
+		
+		Point3D[] in= plane.insideSquare(new Point(25,75),new Point(25,25),
+				new Point(75,75),new Point(75,25));
+		System.out.println(in.length);
+		System.out.println(plane.maxHeight(in).h);
+	}
+	
+	
 
 }
